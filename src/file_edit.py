09011e7e63e416cb09264f00ffc9adcc634c3736
arunima1319 <arunimakumar7@gmail.com>
2026-03-31 16:34:32 +0530

    add command to generate scale practice across octaves

--- src/file_edit.py
+++ src/file_edit.py
@@ -28,7 +28,25 @@ def file_edit(command, file):
             with open (file_path, "w") as f:
                 f.write("")
 
+        case("singing"): 
+            with open (file_path, "r") as f:
+                melody = f.read()
+            melody_lines = melody.split("\n")
+            new_melody_lines = []
+            for i in range (-12, 12, 1):
+                for melody_line in melody_lines: 
+                    new_melody_line = transpose(melody_line, i)
+                    new_melody_lines.append(new_melody_line)
 
+            new_text = ("\n").join(new_melody_lines)
+
+            new_filename = "sing" + "_" + file.split(".")[0] + ".txt"
+            new_file_path = os.path.join("melodies", new_filename)
+
+            with open(new_file_path, "w") as f:
+                f.write(new_text)
+                
+                    
 
     if command[0:9] == "transpose": 
         semitones = int(command[9:])
